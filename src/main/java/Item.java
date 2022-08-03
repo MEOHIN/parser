7a09a15bc43e73f3117224e2ba57fb777faf4f2f
meohin <meohin.meo@gmail.com>
2022-08-03 15:37:39 +0900

    Add getter and setter to read and save

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -10,4 +10,28 @@ public class Item {
     private String name;
     private int length;
     private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
