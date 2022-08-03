63c3913d653fafb38722a84dc6e421e13e546fbf
meohin <meohin.meo@gmail.com>
2022-08-03 16:57:44 +0900

    Add method to add and get Item to the Packet

--- src/main/java/parse/Packet.java
+++ src/main/java/parse/Packet.java
@@ -8,5 +8,26 @@ import java.util.Arrays;
  * 이 클래스는 여러개의 아이템을 포함한다.
  */
 public class Packet {
+    /**
+     * Item 객체를 포함하는 item 속성을 추가한다.
+     * Item 갯수는 가변이므로 ArrayList 에 담았다.
+     */
     private ArrayList<Item> items = new ArrayList<>();
+
+    /**
+     * Item 을 리스트 items 에 추가한다.
+     * @param item 아이템
+     */
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    /**
+     * Item 을 가져온다.
+     * @param index items 리스트의 순번
+     * @return items 리스트에서 입력받은 순번에 있는 item 을 반환한다.
+     */
+    public Item getItem(int index) {
+        return this.items.get(index);
+    }
 }
