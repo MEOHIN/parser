71fe0d7ca9b84e026ff3b99f6d05e43ad3ee1be2
meohin <meohin.meo@gmail.com>
2022-08-03 17:07:40 +0900

    Add method to return text of Packet

--- src/main/java/parse/Packet.java
+++ src/main/java/parse/Packet.java
@@ -30,4 +30,34 @@ public class Packet {
     public Item getItem(int index) {
         return this.items.get(index);
     }
+
+    /**
+     * 이 메소드는 items 의 항목을 하나씩 Item 클래스의 raw() 메소드에 대입해서 반환된 값을 모두 더한 Packet 의 전문을 반환한다.
+     * @return Packet 의 전문을 반환한다.
+     */
+    public String raw() {
+        StringBuilder result = new StringBuilder();
+        for (Item item : items) {
+            result.append(item.raw());
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        item1.setName("이름");
+        item1.setLength(20);
+        item1.setValue("홍길동");
+
+        Item item2 = new Item();
+        item2.setName("전화번호");
+        item2.setLength(11);
+        item2.setValue("01011112222");
+
+        Packet packet = new Packet();
+        packet.addItem(item1);
+        packet.addItem(item2);
+
+        System.out.printf("[%s]", packet.raw());
+    }
 }
